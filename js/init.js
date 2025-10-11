5a914dfd89ac52ab86eb2c6713e0a3ae2a401b3e
Yavin Five <yavinfive464@gmail.com>
2025-10-11 03:30:35 -0300

    bugfix: More JS bug fixes.

--- js/init.js
+++ js/init.js
@@ -1,6 +1,12 @@
-import { saveGithubAccessToken, viewChain, processTxns, fetchState } from './blockchain.js';
+import { saveGithubAccessToken, viewChain, processTxns, fetchState } from './blockchain.js?v=6';
 
 console.log('Imported functions:', { saveGithubAccessToken, viewChain, processTxns, fetchState });
+console.log('Window object before assignments:', {
+    windowSaveGithubAccessToken: window.saveGithubAccessToken,
+    windowViewChain: window.viewChain,
+    windowProcessTxns: window.processTxns,
+    windowFetchState: window.fetchState
+});
 
 try {
     // Expose functions to global scope for main.js
@@ -8,6 +14,20 @@ try {
     window.viewChain = viewChain;
     window.processTxns = processTxns;
     window.fetchState = fetchState;
+    console.log('Window assignments completed:', {
+        windowSaveGithubAccessToken: window.saveGithubAccessToken,
+        windowViewChain: window.viewChain,
+        windowProcessTxns: window.processTxns,
+        windowFetchState: window.fetchState
+    });
 } catch (error) {
     console.error('Error setting global functions:', error);
 }
+
+// Verify assignments after try-catch
+console.log('Final window assignments:', {
+    windowSaveGithubAccessToken: window.saveGithubAccessToken,
+    windowViewChain: window.viewChain,
+    windowProcessTxns: window.processTxns,
+    windowFetchState: window.fetchState
+});
