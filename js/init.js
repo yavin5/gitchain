4a19efe8bbf0891a2de49e9b42dba08f389d4c07
Yavin Five <yavinfive464@gmail.com>
2025-10-11 03:19:35 -0300

    bugfix: More JS fixes.

--- js/init.js
+++ js/init.js
@@ -1,11 +1,13 @@
+import { saveGithubAccessToken, viewChain, processTxns, fetchState } from './blockchain.js';
+
+console.log('Imported functions:', { saveGithubAccessToken, viewChain, processTxns, fetchState });
+
 try {
-    import { saveGithubAccessToken, viewChain, processTxns, fetchState } from './blockchain.js';
-    console.log('Imported functions:', { saveGithubAccessToken, viewChain, processTxns, fetchState });
     // Expose functions to global scope for main.js
     window.saveGithubAccessToken = saveGithubAccessToken;
     window.viewChain = viewChain;
     window.processTxns = processTxns;
     window.fetchState = fetchState;
 } catch (error) {
-    console.error('Error importing or setting global functions:', error);
+    console.error('Error setting global functions:', error);
 }
