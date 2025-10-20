a79636a1e76ae814a3df3f373df9f20798e27746
Yavin Five <yavinfive464@gmail.com>
2025-10-20 02:40:24 -0300

    bugfix: Debugging server-peer.json put.

--- js/bundle.js
+++ js/bundle.js
@@ -33244,10 +33244,8 @@ async function updateServerPeers() {
     console.log("data content: " + data.content);
     if (data.content) {
       console.log("base64 decoding and JSON parsing content.");
-      data = btoa(data.content);
+      data = atob(data.content);
       console.log("base64 decoded: " + data);
-      data = btoa(data.content);
-      console.log("base64 decoded 2nd time: " + data);
       data = JSON.parse(data);
     }
     console.log("data: " + data);
