4540e21d55d2462cbc2bed5bca1ebc8cc2a9a923
Yavin Five <yavinfive464@gmail.com>
2025-11-10 01:04:20 -0300

    bugfix: Fixing wallet creation via kasstamp SDK.

--- src/chain.ts
+++ src/chain.ts
@@ -54,6 +54,7 @@ import {
   type TransactionEvent,
 } from '@kasstamp/sdk';
 import * as WASM from '@kasstamp/kaspa_wasm_sdk';
+console.log(WASM);
 //var w = WASM;
 //import { WalletService, walletService } from './WalletService';
 //import type { WalletServiceEvent, WalletServiceEventData } from './WalletService';
